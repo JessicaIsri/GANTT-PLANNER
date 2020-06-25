e77c8bc89011d4e439472b533d575f3b923ce5b5
giovanniguidace <giovannimarassi@gmail.com>
2020-06-24 22:58:55 -0300

    criacao de drag and drop para geracao de dashboard

--- pi/gantt/static/js/pi_relatorio.js
+++ pi/gantt/static/js/pi_relatorio.js
@@ -28,11 +28,26 @@ function carregaProjetos(){
                 document.getElementById("projetos_cadastrados").innerHTML = '';
                 for(i = 0; i< json_projetos_cadastrados.length;i++){
 
-                    linha = "<button class='btn_shadow4_2' style='background-color:"+json_projetos_cadastrados[i]['prj_color']+"'>"+json_projetos_cadastrados[i]['prj_nome']+"</button>";
+                    linha = "<button id='projeto_id"+json_projetos_cadastrados[i]['prj_id']+"' class='btn_shadow4_2' draggable=true ondragstart='return dragStart(event)' ondragend='return dragEnd(event)' style='background-color:"+json_projetos_cadastrados[i]['prj_color']+"'>"+json_projetos_cadastrados[i]['prj_nome']+"</button>";
                     document.getElementById("projetos_cadastrados").innerHTML += linha;
                 }
             }
         }
     }
     xhrGetProjetos.send();
+}
+
+function dragStart(ev) {
+    ev.dataTransfer.setData("ID",ev.target.getAttribute('id'));
+}
+function dragOver(ev) { 
+    return false;
+}
+function dragDrop(acao) {
+    produtoSelecionado = acao.dataTransfer.getData("ID");
+    acao.target.appendChild(document.getElementById(produtoSelecionado));
+}
+function dragEnd(acao) {
+    acao.dataTransfer.clearData("ID");
+    return true;
 }
