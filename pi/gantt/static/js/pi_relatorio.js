b4052d894fc225460052e1d7d49c3a6c98eaccbc
Giovanni Guidace <giovannimarassi@gmail.com>
2020-06-25 15:57:17 -0300

    preparando dashboard

--- pi/gantt/static/js/pi_relatorio.js
+++ pi/gantt/static/js/pi_relatorio.js
@@ -3,6 +3,7 @@
 /**ESTE ARQUIVO JAVASCRIPT FOI PROJETADO PARA TRATAR APENAS O RELATÓRIO*/
 
 
+/**TELA INICIAL - RELATÓRIO */
 function clicaRelatorio(){
     dialogCadastro = document.getElementById("abreRelatorio");
     dialogPolyfill.registerDialog(dialogCadastro);
@@ -73,6 +74,9 @@ function dragEnd(acao) {
 
     
 }
+/**/////////////////////////////////////////////////////////// */
+
+/**TELA DASHBOARD */
 
 projetos_dashboard = [];
 
@@ -90,6 +94,10 @@ function gerarDashboard(){
     dialogPolyfill.registerDialog(dialogCadastro);
     dialogCadastro.showModal();
 
+    getProjetosDashboard();
+    getTarefasDashboard();
+    getPessoasDashboard();
+    dadosDashboard();
 
     /*for(i=0;i<projetos_dashboard.length;i++){
 
@@ -98,9 +106,109 @@ function gerarDashboard(){
 
 
 }
-
 function fecharDashboard(){
     dialogCadastro.close();
-    dialogCadastro = document.getElementById("abreDashboard");
+    dialogCadastro = document.getElementById("abreRelatorio");
     dialogPolyfill.registerDialog(dialogCadastro);
+}
+
+
+/**ÁREA DE PROJECAO DO DASHBOARD */
+
+function getProjetosDashboard(){
+    xhrGetProjetos = new XMLHttpRequest();
+
+    xhrGetProjetos.open('GET', URLGETPROJETOS, true);
+
+    xhrGetProjetos.onreadystatechange = function(){
+        if(xhrGetProjetos.readyState == 4){
+            if(xhrGetProjetos.status == 200){
+                json_get_projetos = JSON.parse(xhrGetProjetos.responseText);
+
+                dadosDashboard(json_get_projetos ,null, null);
+
+            }
+        }
+    }
+    xhrGetProjetos.send();
+}
+
+function getTarefasDashboard(){
+    xhrGetTarefas = new XMLHttpRequest();
+
+    xhrGetTarefas.open('GET', URLGETTAREFAS, true);
+
+    xhrGetTarefas.onreadystatechange = function(){
+        if(xhrGetTarefas.readyState == 4){
+            if(xhrGetTarefas.status == 200){
+                json_get_tarefas = JSON.parse(xhrGetTarefas.responseText);
+
+                dadosDashboard(null , json_get_tarefas, null);
+            }
+        }
+    }
+    xhrGetTarefas.send();
+
+}
+
+function getPessoasDashboard(){
+    xhrGetPessoas = new XMLHttpRequest();
+
+    xhrGetPessoas.open('GET', URLGETPESSOAS, true);
+
+    xhrGetPessoas.onreadystatechange = function(){
+        if(xhrGetPessoas.readyState == 4){
+            if(xhrGetPessoas.status == 200){
+                json_get_pessoas = JSON.parse(xhrGetPessoas.responseText);
+
+                dadosDashboard(null , null, json_get_pessoas);
+                
+            }
+        }
+    }
+    xhrGetPessoas.send();
+}
+
+recebe_dados_projetos = [];
+recebe_dados_tarefas = [];
+recebe_dados_pessoas = [];
+
+function dadosDashboard(json_get_projetos, json_get_tarefas, json_get_pessoas){
+
+    if(json_get_projetos != null){
+        recebe_dados_projetos = json_get_projetos;
+    }
+    if(json_get_tarefas != null){
+        recebe_dados_tarefas = json_get_tarefas;
+    }
+
+    if(json_get_pessoas != null){
+        recebe_dados_pessoas = json_get_pessoas;
+    }
+
+    console.log(recebe_dados_projetos);
+    console.log(recebe_dados_tarefas);
+    console.log(recebe_dados_pessoas);
+
+    for(i=0; i<recebe_dados_projetos.length;i++){
+        documento.getElementById('dashboard_projetos').innerHTML = '';
+        linha_projeto = "<div id='prj"+recebe_dados_projetos[i]['prj_id']+"' class='view_projeto'><label class='styleWord1'>"+recebe_dados_projetos[i]['prj_id']+"</label></div>"
+        documento.getElementById('dashboard_projetos').innerHTML +=  linha_projeto;
+        
+        documento.getElementById('prj'+recebe_dados_projetos[i]['prj_id']+'').innerHTML = '';
+        linha_dados_projeto = "<div id='dados_projeto"+recebe_dados_projetos[i]['prj_id']+"' class='class_dados_projeto'>|<br>------><label id='lb_dados_projeto"+recebe_dados_projetos[i]['prj_id']+"'>DADOS PROJETO --> Progresso: "+recebe_dados_projetos[i]['prj_progresso']+" Data Início: "+recebe_dados_projetos[i]['prj_datainicio']+" Prazo Entrega: "+recebe_dados_projetos[i]['prj_prazoentrega']+" Horas Desenvolvimento: "+recebe_dados_projetos[i]['prj_hrs_dev']+" Custo Projeto: "+recebe_dados_projetos[i]['prj_cost']+"</label></div>";
+        documento.getElementById('prj'+recebe_dados_projetos[i]['prj_id']+'').innerHTML += linha_dados_projeto;
+
+        for(x=0; x<recebe_dados_tarefas.length;x++){
+            
+
+
+            for(z=0;z<recebe_dados_pessoas.length;z++){
+
+            }
+        }
+    }
+
+
+
 }
