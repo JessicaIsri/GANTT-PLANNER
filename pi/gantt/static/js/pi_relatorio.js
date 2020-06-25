6ffeb3ddafc9a9fc4c37934c69d92658e74a0f8b
giovanniguidace <giovannimarassi@gmail.com>
2020-06-24 22:32:50 -0300

    preparando relatorio

--- pi/gantt/static/js/pi_relatorio.js
+++ pi/gantt/static/js/pi_relatorio.js
@@ -7,8 +7,32 @@ function clicaRelatorio(){
     dialogCadastro = document.getElementById("abreRelatorio");
     dialogPolyfill.registerDialog(dialogCadastro);
     dialogCadastro.showModal();
+
+    carregaProjetos();
 }
 
 function fecharRelatorio(){
     dialogCadastro.close();
+}
+
+function carregaProjetos(){
+    xhrGetProjetos = new XMLHttpRequest();
+
+    xhrGetProjetos.open('GET', URLGETPROJETOS, true);
+
+    xhrGetProjetos.onreadystatechange = function(){
+        if(xhrGetProjetos.readyState == 4){
+            if(xhrGetProjetos.status == 200){
+
+                json_projetos_cadastrados = JSON.parse(xhrGetProjetos.responseText);
+                document.getElementById("projetos_cadastrados").innerHTML = '';
+                for(i = 0; i< json_projetos_cadastrados.length;i++){
+
+                    linha = "<button class='btn_shadow4_2' style='background-color:"+json_projetos_cadastrados[i]['prj_color']+"'>"+json_projetos_cadastrados[i]['prj_nome']+"</button>";
+                    document.getElementById("projetos_cadastrados").innerHTML += linha;
+                }
+            }
+        }
+    }
+    xhrGetProjetos.send();
 }
